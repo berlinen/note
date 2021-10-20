6958e96fdbac06a2781114a2ca9c7d47f081e65e
berlinen <1292393108@qq.com>
2021-10-21 00:43:21 +0800

    feat: 实现副作用函数

--- vue-next/packages/reactivity/src/effect.ts
+++ vue-next/packages/reactivity/src/effect.ts
@@ -2,6 +2,7 @@
 let uid = 0
 let activeEffect // 存储当前的effect
 let effectStack = [] // effect 一个栈 根据函数调用执行站
+const targetMap = new WeakMap()
 
 const effect = (fn, options:any = {}) => {
   // 我需要让这个effect编程响应的effect， 可以做到重新执行
@@ -17,13 +18,15 @@ const effect = (fn, options:any = {}) => {
 
 const createReactiveEffect = (fn, options) => {
   const effect = function reactiveEffect() {
-   try {
-    effectStack.push(effect)
-    activeEffect = effect
-    fn() //函数执行时候会取之 会执行get函数
-   } finally {
-     effectStack.pop()
-     activeEffect = effectStack[effectStack.length - 1]
+   if(!effectStack.includes(effect)) { // 保证effect 没有加入到effectStack中
+    try {
+      effectStack.push(effect)
+      activeEffect = effect
+      return fn() //函数执行时候会取之 会执行get函数
+     } finally {
+       effectStack.pop()
+       activeEffect = effectStack[effectStack.length - 1]
+     }
    }
   }
 
@@ -35,9 +38,30 @@ const createReactiveEffect = (fn, options) => {
   return effect
 }
 
-const track = (target, type, key) => {
+// 让某刻对象中的属性收集当前他对应的effect函数
+const track = (target, type, key) => { // 可以拿到当前的effect
+  // activeEffect // 当前正在运行的effect
+  if(activeEffect === undefined) return // 此属性不用依赖手机 因为没在effect中使用
+
+  let depsMap = targetMap.get(target)
+
+  if(!depsMap) {
+    targetMap.set(target, (depsMap = new Map))
+  }
+
+  let dep = depsMap.get(key)
+
+  if(!dep) {
+    depsMap.set(key, (dep = new Set))
+  }
+
+  if(!dep.has(activeEffect)) {
+    dep.add(activeEffect)
+  }
 
 }
+
+// {name: 'berlon', age: 12} => name => [effect, effect]
 
 export {
   effect,
