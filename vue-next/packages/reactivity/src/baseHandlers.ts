3abe0f23fc7fc9119da5de1d1f4642f764d276ba
berlinen <1292393108@qq.com>
2021-10-20 23:12:42 +0800

    feat: 设置拦截

--- vue-next/packages/reactivity/src/baseHandlers.ts
+++ vue-next/packages/reactivity/src/baseHandlers.ts
@@ -1,9 +1,16 @@
 // 实现new Proxy(target, handlers)
 
+import { extend } from '@vue/shared'
 // 是不是仅读的 设置 set时候会报异常
 // 是不是深度的
 
+// 核心拦截获取
 const createGetter = (isReadonly = false, isShallow = false) => {
+
+}
+
+// 拦截设置值
+const createSetter = (isShallow = false) => {
 
 }
 
@@ -12,21 +19,32 @@ const shallowGet = createGetter(false, true)
 const readonlyGet = createGetter(true, false)
 const shallowReadonlyGet = createGetter(true, true)
 
+const set = createSetter()
+const shallowSet = createSetter(true)
+
+let readonlyObj = {
+  set: (target, key, value) => {
+    console.log(`can not set ${key} equal ${value} on target ${target}`)
+  }
+}
+
 const mutableHandlers = {
   get,
+  set
 }
 
 const shallowReactiveHandlers = {
-  get: shallowGet
+  get: shallowGet,
+  set: shallowSet
 }
 
-const readonlyHandlers = {
-  get: readonlyGet
-}
+const readonlyHandlers = extend({
+  get: readonlyGet,
+}, readonlyObj)
 
-const shallowReadonlyHandlers = {
+const shallowReadonlyHandlers = extend({
   get: shallowReadonlyGet
-}
+}, readonlyObj)
 
 export {
   mutableHandlers,
--- vue-next/packages/shared/src/index.ts
+++ vue-next/packages/shared/src/index.ts
@@ -1,5 +1,7 @@
 const isObject = val => typeof val === 'object' && val !== null
+const extend = Object.assign
 
 export {
-  isObject
+  isObject,
+  extend
 }
