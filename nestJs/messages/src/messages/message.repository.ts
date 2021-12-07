5f0934064f61e7c586f3e06746c564e79a384498
berlinen <1292393108@qq.com>
2021-12-07 22:32:17 +0800

    feat: init repository

---
+++ nestJs/messages/src/messages/message.repository.ts
@@ -0,0 +1,15 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessageRepository {
+  async findOne(id: string) {
+
+  }
+
+  async findAll() {
+
+  }
+
+  async createMessage(message: string) {
+
+  }
+}
