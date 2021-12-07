cef4703cc42a4c2506f350f88028ed5d3773c3df
berlinen <1292393108@qq.com>
2021-12-07 22:34:14 +0800

    feat: find one

--- nestJs/messages/src/messages/message.repository.ts
+++ nestJs/messages/src/messages/message.repository.ts
@@ -1,8 +1,12 @@
+import { read } from "fs";
 import { readFile, writeFile } from "fs/promises";
 
 export class MessageRepository {
   async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
 
+    return messages[id]
   }
 
   async findAll() {
