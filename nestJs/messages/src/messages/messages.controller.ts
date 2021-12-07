146f075aea34ebd1a45ca0d2c4db29e981d0ec19
berlinen <1292393108@qq.com>
2021-12-07 22:58:12 +0800

    feat: message service

--- nestJs/messages/src/messages/messages.controller.ts
+++ nestJs/messages/src/messages/messages.controller.ts
@@ -1,20 +1,29 @@
 import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './message.service';
 
 @Controller('messages')
 export class MessagesController {
+  mesagesService: MessagesService
+
+  constructor() {
+    // dont do this on real app
+    // use dependency injection
+    this.mesagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-
+    return this.mesagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body)
+    return this.mesagesService.createMessage(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log('id', id)
+    return this.mesagesService.findOne(id);
   }
 }
