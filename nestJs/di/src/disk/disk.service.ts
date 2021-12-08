e90879e20b5024a7cd30951247e72d2e8c9d8dd8
berlinen <1292393108@qq.com>
2021-12-08 22:35:22 +0800

    feat: injectable

--- nestJs/di/src/disk/disk.service.ts
+++ nestJs/di/src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerSerivice: PowerService) {}
+
+  getData() {
+    console.log('Drawing 20 watts of power from PowerService')
+    this.powerSerivice.supplyPower(20)
+    return "data!"
+  }
+}
