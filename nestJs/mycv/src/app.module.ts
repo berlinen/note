0af79e074ace418ef764284280e1fcd2c0420cdc
berlinen <1292393108@qq.com>
2021-12-09 00:02:38 +0800

    feat: 注册实体

--- nestJs/mycv/src/app.module.ts
+++ nestJs/mycv/src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity'
+import { Report } from './reports/report.entity'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [], // 实体
+      entities: [User, Report], // 实体
       synchronize: true
     }),
     UsersModule,
