574bd4b291b50643bebb3eb7289d198afa0bf594
berlinen <1292393108@qq.com>
2021-12-09 23:02:03 +0800

    findone

--- nestJs/mycv/src/users/users.service.ts
+++ nestJs/mycv/src/users/users.service.ts
@@ -8,8 +8,21 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
   create(email: string, password: string) {
+    // 创建实体实例
     const user = this.repo.create({ email, password })
-
+    // 数据持久化 存入数据库 验证之类操作
     return this.repo.save(user)
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id)
+  }
+
+  find(email: string) {
+    return this.repo.find({ email })
+  }
+
+  update() {}
+
+  remove() {}
 }
