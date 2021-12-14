433f155a949d753a85c782888d6303dbabee8fb3
berlinen <1292393108@qq.com>
2021-12-14 23:03:02 +0800

    feat: scrypt hash salt

--- nestJs/mycv/src/users/auth.service.ts
+++ nestJs/mycv/src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadGatewayException, Injectable } from "@nestjs/common";
-import { identity } from "rxjs";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt)
 
 @Injectable()
 export class AuthService {
@@ -12,12 +15,17 @@ export class AuthService {
     if(users.length) {
       throw new BadGatewayException('email in use')
     }
-
     // hase the users password
-
+    // generate a salt
+    // 缓冲区将有8个字节的数据
+    const salt = randomBytes(8).toString('hex')
+    // hash the salt the passwoed together 32 bytes
+    const hash = (await scrypt(password, salt, 32)) as Buffer
+    // join rhe hasheed result and the sale together
+    const result = salt + '.' + hash.toString('hex')
     // create a new user and save it
-
-    // return the user  
+    
+    // return the user
   }
 
   signin() {
