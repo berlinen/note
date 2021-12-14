d3445a8d4cd2c3cc0c18ac4811917420e87f4fa3
berlinen <1292393108@qq.com>
2021-12-15 00:22:28 +0800

    feat: CurrentUserInterCeptor

---
+++ nestJs/mycv/src/users/interceptors/current-user.interceptors.ts
@@ -0,0 +1,18 @@
+import { NestInterceptor, ExecutionContext, CallHandler, Injectable } from "@nestjs/common";
+import { UsersService } from "../users.service";
+
+export class CurrentUserInterCeptor implements NestInterceptor {
+  constructor(private userService: UsersService) {}
+
+  async intercept(ctx: ExecutionContext, handler: CallHandler) {
+
+    const request = ctx.switchToHttp().getRequest()
+    const { userId } = request.session
+
+    if(userId) {
+      const user = await this.userService.findOne(userId)
+    }
+
+    return handler.handle();
+  }
+}
