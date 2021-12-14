289310673afb86ed5f5ae50f71f766388fa074c2
berlinen <1292393108@qq.com>
2021-12-14 22:40:56 +0800

    feat: check email

--- nestJs/mycv/src/users/auth.service.ts
+++ nestJs/mycv/src/users/auth.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { BadGatewayException, Injectable } from "@nestjs/common";
+import { identity } from "rxjs";
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // see id email in use
+    const users = await this.userService.find(email)
+    if(users.length) {
+      throw new BadGatewayException('email in use')
+    }
+
+    // hase the users password
+
+    // create a new user and save it
+
+    // return the user  
+  }
+
+  signin() {
+
+  }
 }
