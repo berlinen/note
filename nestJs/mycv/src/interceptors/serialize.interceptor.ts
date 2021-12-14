3ff625969953347bbfc9435ed3e14abbedf49215
berlinen <1292393108@qq.com>
2021-12-14 21:34:03 +0800

    feat: nest userDto

--- nestJs/mycv/src/interceptors/serialize.interceptor.ts
+++ nestJs/mycv/src/interceptors/serialize.interceptor.ts
@@ -2,6 +2,7 @@ import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from 
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { plainToClass } from 'class-transformer'
+import { UserDto } from 'src/users/dtos/user.dto'
 
 export class SerializerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
@@ -10,9 +11,10 @@ export class SerializerInterceptor implements NestInterceptor {
     // console.log('Im running before the handler', context) first
     return handler.handle().pipe(
       map((data: any) => {
-        // run something before the response is sent out
-        console.log('Im runing before response is sent out', data) // third
-        return data
+        // run something before the response is sent out  third
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true
+        })
       })
     )
   }
