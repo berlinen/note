843591c4e23a4c871fe3ecc4f8ba077c5f23d5d6
berlinen <1292393108@qq.com>
2021-12-15 22:12:09 +0800

    feat: test

--- nestJs/mycv/src/users/auth.service.spec.ts
+++ nestJs/mycv/src/users/auth.service.spec.ts
@@ -5,10 +5,11 @@ import { User } from './user.entity'
 
 describe('AuthService', () => {
   let service: AuthService
+  let fakeUsersService: Partial<UsersService>
 
   beforeEach(async() => {
     // create a fake copy of the users service
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
     }
@@ -36,6 +37,16 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.')
     expect(salt).toBeDefined()
     expect(hash).toBeDefined()
+  })
+
+  it('throws an error if user sign up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'abc@test.com', password: '123456'} as User])
+    try {
+      await service.signup('129239@qq.com	', '123456')
+    } catch(err) {
+
+    }
   })
 })
 
