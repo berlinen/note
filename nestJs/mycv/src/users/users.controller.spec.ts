735516882f834c4e7319733cff219cfc98fb185c
berlinen <1292393108@qq.com>
2021-12-15 23:18:15 +0800

    feat: test

--- nestJs/mycv/src/users/users.controller.spec.ts
+++ nestJs/mycv/src/users/users.controller.spec.ts
@@ -44,4 +44,10 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('findAllUsers returns a list of users with the given email', async() => {
+    const users = await controller.findAllUsers('aa@test.com')
+    expect(users.length).toEqual(1)
+    expect(users[0].email).toEqual('aa@test.com')
+  })
 });
