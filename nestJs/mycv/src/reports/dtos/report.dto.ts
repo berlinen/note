55468296c837a700788c7f5fa6d1fa30ffd46b46
berlinen <1292393108@qq.com>
2021-12-17 00:09:17 +0800

    userID

---
+++ nestJs/mycv/src/reports/dtos/report.dto.ts
@@ -0,0 +1,34 @@
+import { Expose, Transform } from "class-transformer";
+import { User } from "../../users/user.entity";
+
+export class ReportDto {
+  @Expose()
+  id: number
+
+  @Expose()
+  make: string
+
+  @Expose()
+  model: string
+
+  @Expose()
+  year: number
+
+  @Expose()
+  mileage: number
+
+  @Expose()
+  lng: number
+
+  @Expose()
+  price: number
+
+  @Expose()
+  lat: number
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number
+
+
+}
