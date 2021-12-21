ffc922e70292f668e3e00b169c474071a710c848
berlinen <1292393108@qq.com>
2021-12-22 00:16:26 +0800

    feat: nest  创建查询

--- nestJs/mycv/src/reports/reports.service.ts
+++ nestJs/mycv/src/reports/reports.service.ts
@@ -10,10 +10,17 @@ import { GetEstimateDto } from './dtos/get-estimate.dto';
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report> ) {}
 
-  createEstimate(estimateDto: GetEstimateDto) {
+  createEstimate({ make, model, lng, lat, year, mileage }) {
     return this.repo.createQueryBuilder()
       .select('*')
-      .where('make = :make', { make: estimateDto.make})
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :l at BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      // @ts-ignore
+      .setParameter({ mileage })
       .getRawMany()
   }
 
