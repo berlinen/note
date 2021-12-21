8f09c6fadca185425e12b540601eb2f73b1e5143
berlinen <1292393108@qq.com>
2021-12-21 23:35:09 +0800

    fix ts error

--- nestJs/mycv/src/users/middlewares/current-user.middleware.ts
+++ nestJs/mycv/src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,15 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from 'express'
 import { UsersService } from "../users.service";
+import { User } from '../user.entity'
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User
+    }
+  }
+}
 
 Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -15,7 +24,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     if(userId) {
       console.log('>>>this.usersService>>', this.usersService)
       const user = await this.usersService.findOne(userId)
-      // @ts-ignore
       req.currentUser = user
     }
 
